9e26d40e61221b44cc9ed0b933c6c2e3d0bf1a0f
ShifaAL-harasi <ShifaAL-harasi98@outlook.com>
2021-09-27 15:04:01 +0400

    BookProject

---
+++ ListOfBookss/Controllers/BookController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ListOfBookss.Models;
+
+namespace ListOfBookss.Controllers
+{
+    public class BookController : Controller
+    {
+        private ListOfBooksEntities db = new ListOfBooksEntities();
+
+        // GET: Book
+        public ActionResult Index()
+        {
+            return View(db.BookMasters.ToList());
+        }
+
+        // GET: Book/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BookMaster bookMaster = db.BookMasters.Find(id);
+            if (bookMaster == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bookMaster);
+        }
+
+        // GET: Book/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Book/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,BookNumber,BookName,Author,Price")] BookMaster bookMaster)
+        {
+            if (ModelState.IsValid)
+            {
+                db.BookMasters.Add(bookMaster);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(bookMaster);
+        }
+
+        // GET: Book/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BookMaster bookMaster = db.BookMasters.Find(id);
+            if (bookMaster == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bookMaster);
+        }
+
+        // POST: Book/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,BookNumber,BookName,Author,Price")] BookMaster bookMaster)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bookMaster).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bookMaster);
+        }
+
+        // GET: Book/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BookMaster bookMaster = db.BookMasters.Find(id);
+            if (bookMaster == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bookMaster);
+        }
+
+        // POST: Book/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BookMaster bookMaster = db.BookMasters.Find(id);
+            db.BookMasters.Remove(bookMaster);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
